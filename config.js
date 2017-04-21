e4f1681eb584bb5bc30cf5cc54f11576b81e08e2
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-04-21 15:19:27 +0100

    adds in config js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = (function() {
+  var config = {
+        clinicService: {
+            url: "http://data.gov.uk/data/api/service/health/clinics?city="
+        }
+    };
+    return config;
+})();
