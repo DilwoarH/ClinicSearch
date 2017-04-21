194de1bbbe78076e1a89baa5c163648d924053d0
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-04-21 16:24:50 +0100

    transformations, error handling and lodash added

--- controllers/ClinicsController.js
+++ controllers/ClinicsController.js
@@ -3,19 +3,44 @@ require('../services/ClinicsService')
 module.exports = ClinicsController = (function() {
   function ClinicsController() {}
 
-  ClinicsController.prototype.getClinicsByLocationName = ( Location ) => {
+  ClinicsController.prototype.getClinicsByLocationName = function( location ) {
 
-      return new Promise((resolve, reject) => {
+      var _this = this;
+
+      return new Promise(function(resolve, reject){
 
           var _clinicsService     = new ClinicsService();
 
-          return _clinicsService.getClinics( Location )
+          return _clinicsService.getClinics( location )
           .then( ( res ) => {
-            resolve( res );
+            var transformedResponse = _this.transformResponse( res );
+            resolve(transformedResponse);
+          })
+          .catch( (err) => {
+
+            var _errMessage = { 
+              status: "error", 
+              message: err.message 
+            };
+
+
+            resolve( _errMessage );
           });
 
         
       });
+  };
+
+  ClinicsController.prototype.transformResponse = ( res ) => {
+
+    var transformedResponse = {
+      results: {},
+      total: 0
+    };
+
+    
+
+    return res;
   };
 
   return ClinicsController;
