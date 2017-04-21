8797e22600208bf3c086b825ceb430b94f1cd371
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-04-21 15:21:46 +0100

    changes to syntax

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-require('./controllers/ClinicsController')
+require('./controllers/ClinicsController');
 
-app.get('/clinics/city/:name', function (req, res) {
+app.get('/clinics/city/:name', (req, res) => {
     var _clinicsController =  new ClinicsController();
     
     _clinicsController.get( req.params.name )
-    .then( function( response ){
+    .then( ( response ) => {
       res.send( response );
-    } );
+    });
     
-})
+});
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Application listening on port 3000! Go to http://localhost:3000.')
-})
+});
