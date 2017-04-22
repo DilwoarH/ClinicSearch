a726d92d1e5129f21bd635384948bdfc71840992
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-04-22 21:44:51 +0100

    Adds comments

--- controllers/ClinicsController.js
+++ controllers/ClinicsController.js
@@ -1,5 +1,5 @@
-var _ = require('lodash');
-require('../services/ClinicsService');
+var _ = require('lodash'); // requires lodash for _. functions
+require('../services/ClinicsService'); // requires ClinicsService JS file
 
 module.exports = ClinicsController = (function() {
 
@@ -22,31 +22,39 @@ module.exports = ClinicsController = (function() {
   *********************************/
   ClinicsController.prototype.getClinicsByLocationName = function( location ) {
 
+      // pointer for main scope
       var _this = this;
 
       return new Promise(function(resolve, reject){
 
+          // create new instance of ClinicsService class
           var _clinicsService     = new ClinicsService();
 
           return _clinicsService.getClinics( location )
           .then( ( res ) => {
             
+            /* 
+              this is to handle cases where ISPs intercept a "Host not found" / 
+              DNS failure and respond with a 200 error and a HTTP response.
+              (TESTED on BT Broadband)
+            */
             if ( !_.isObject( res.body ) || res.status === "error" )
             {
-                throw new Error(res.message);
+                throw new Error(res.message); // throw error so catch functionality intercepts
             }
 
-            var transformedResponse = _this.transformResponse( res.body );
-            resolve(transformedResponse);
+            var transformedResponse = _this.transformResponse( res.body ); // transform response to specification requirements
+            resolve(transformedResponse); // resolve promise with transformed response
           })
           .catch( (err) => {
 
+            // generate error message with status error
             var _errMessage = { 
-              status: "error", 
-              message: err.message 
+              status: "error", // uses status error
+              message: err.message  // send error message from caught error
             };
 
-            resolve( _errMessage );
+            resolve( _errMessage ); // resolve with error message
           });
 
       });
@@ -67,32 +75,38 @@ module.exports = ClinicsController = (function() {
         -> return transformed results
   *********************************/
   ClinicsController.prototype.transformResponse = ( res ) => {
+    
+    // intialise transformed response object
     var transformedResponse = {
       status: "success", // result status
       results: {}, //an object with all the partial_postcodes found on the results and how many of them where found
       total: 0 //the total number of different partial_postcodes found
     };
 
+    // loops through each results and picks out the partial postcode and adds it to the count
     for( i in res.result )
     {
-      var clinic = res.result[i];
+      var clinic = res.result[i]; // stores object in local variable called clinic for readablity 
 
+      // if postcode has not been counted yet then set initial count, else, add to existing count
       if ( _.isUndefined( transformedResponse.results[ clinic.partial_postcode ] ) )
       {
-          transformedResponse.results[ clinic.partial_postcode ] = 1;
+          transformedResponse.results[ clinic.partial_postcode ] = 1; // init from 1
       }
       else
       {
-          transformedResponse.results[ clinic.partial_postcode ]++;
+          transformedResponse.results[ clinic.partial_postcode ]++; // add 1 to existing count
       }
 
     }
 
+    // count the number of unique postcode for total - specification requirements
     transformedResponse.total = _.size( transformedResponse.results );
 
+    // return transformed response
     return transformedResponse;
   };
-
+  
   return ClinicsController;
 
 })();
